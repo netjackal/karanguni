957d8aadefac34a93dca29087cd8b66fd6ae009e
Kelvin Teh <register@netjackal.net>
2011-08-04 03:09:48 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Karanguni::Application.config.secret_token = '3b3d192f5bba8b5ee2e659a38251fc3e5919a140db3b86739ad3aa602a5103fcde8b6ad3d562ddcc322503fb72e0916030c537a4f15970289ee4fdb6306966a8'
